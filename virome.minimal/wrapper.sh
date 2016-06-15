36d136527aab6df992218f62a834836c234a7a6a
Tom Emmel <temmel@temmel-osx.local>
2016-06-15 09:33:13 -0400

    added data download options

--- virome.minimal/wrapper.sh
+++ virome.minimal/wrapper.sh
@@ -1,16 +1,63 @@
-#!/bin/bash
+#!/bin/sh
 
-export PERL5LIB=/opt/package_virome/autopipe_package/ergatis/lib
+
+usage() {
+
+	echo "Usage: $0 [OPTIONS]"
+	echo "  --enable-data-download      perform data file download (default)"
+	echo "  --disable-data-download     do not perform data file download"
+	echo "  -h, --help                  display this help and exit"
+}
+
+#--------------------------------------------------------------------------------
+# Process parameters
+
+opt_d=1
+
+while true
+do
+	case $1 in
+
+	-h|--help)
+		usage
+		exit
+		;;
+	--enable-data-download)
+		opt_d=1
+		;;
+	--disable-data-download)
+		opt_d=0
+		;;
+		--)
+		shift
+		break
+		;;
+	-?*)
+		echo "$0: invalid option: $1"
+		exit 1
+		;;
+	*)
+		break
+	esac
+
+	shift
+done
+
+if [ $# != 0 ]
+then
+	usage
+	exit 1
+fi
 
 #--------------------------------------------------------------------------------
 # Verify input/output/project directories
 
-### if [ ! -d /tmp/input ]
-### then
-### 	echo "$0: directory not found: /tmp/input"
-### 	exit 1
-### fi
-### 
+if [ ! -d /tmp/input ]
+then
+	echo "$0: directory not found: /tmp/input"
+	exit 1
+fi
+
 ### if [ ! -d /tmp/output ]
 ### then
 ### 	echo "$0: directory not found: /tmp/output"
@@ -26,34 +73,40 @@ export PERL5LIB=/opt/package_virome/autopipe_package/ergatis/lib
 #--------------------------------------------------------------------------------
 # Download data files
 
-### cd /tmp/input
-### 
-### DATA_FILES="\
-### 	MGOL_DEC2014 \
-### 	MGOL_DEC2014.60 \
-### 	UNIREF50_2015_12 \
-### 	UNIREF100_2015_12 \
-### 	UniVec_Core \
-### 	rRNA \
-### 	mgol60__2__mgol100.lookup \
-### 	uniref50__2__uniref100.lookup"
-### 
-### for file in $DATA_FILES
-### do
-### 	echo "`date`: $file"
-### 	zsync -q http://virome.dbi.udel.edu/repository/$file.zsync
-### 	test -s $file.zs-old && /bin/rm $file.zs-old
-### 	chmod 644 $file
-### done
-### echo `date`
+if [ $opt_d -eq 1 ]
+then
+	cd /tmp/input
+	
+	DATA_FILES="\
+		MGOL_DEC2014 \
+		MGOL_DEC2014.60 \
+		UNIREF50_2015_12 \
+		UNIREF100_2015_12 \
+		UniVec_Core \
+		rRNA \
+		mgol60__2__mgol100.lookup \
+		uniref50__2__uniref100.lookup"
+	
+	for file in $DATA_FILES
+	do
+		echo "start: `date`: $file"
+		zsync -q http://virome.dbi.udel.edu/repository/$file.zsync
+		test -s $file.zs-old && /bin/rm $file.zs-old
+		test -s $file && chmod 644 $file
+	done
+	echo "completed: `date`"
+fi
 
 #--------------------------------------------------------------------------------
 # Configure/run pipeline (virome)
 
-# TODO: This next step is probably asynchronousk, so it probably immediately exits
-# after the pipeline is executed. So, we need to invoke another script to 
+export PERL5LIB=/opt/package_virome/autopipe_package/ergatis/lib
+
+# TODO: This next step is probably asynchronous, so it probably immediately exits
+# after the pipeline is executed. So, we need to invoke another script to
 # block and wait for the pipeline to be complete before this script is allowed
 # to exit
+
 /opt/package_virome/autopipe_package/ergatis/util/virome_little_run_pipeline.pl \
 -t /opt/package_virome/project_saved_templates/little-pipeline/ \
 -r /opt/projects/virome/ \
@@ -66,4 +119,5 @@ export PERL5LIB=/opt/package_virome/autopipe_package/ergatis/lib
 
 # TODO: Implement
 # /opt/scripts/monitor.pl
-exit $?
+
+echo $?
